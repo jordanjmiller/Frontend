6c6704fc02854f8277db7a732baff075d74a2411
Jordan Miller <jordan.j.miller@live.com>
2019-11-19 11:40:00 -0800

    Cleaned up file tree a bit

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,9 +5,9 @@ import SidebarNav from './SidebarNav.js';
 import Account from './Account.js';
 import CreateTicket from './CreateTicket.js';
 
-import OpenTicketList from './OpenTicketList.js';
-import UserTicketList from './UserTicketList.js';
-import ClosedTicketList from './ClosedTicketList.js';
+import OpenTicketList from './Unassigned/OpenTicketList.js';
+import UserTicketList from './Mine/UserTicketList.js';
+import ClosedTicketList from './Closed/ClosedTicketList.js';
 import Ticket from './Ticket';
 
 import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
