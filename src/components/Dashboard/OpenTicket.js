c1fd9ec4312fb8b5b77e8a09790aa98083f1aaf0
William Sulinski <w@williamsulinski.com>
2019-11-19 10:44:07 -0800

    Add Ticket.js, routes and links

--- src/components/Dashboard/OpenTicket.js
+++ src/components/Dashboard/OpenTicket.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import * as timeago from 'timeago.js';
+import Ticket from './Ticket';
 
 export default function OpenTicket(props) {
     return (
         <>
-            <td className='boldrows'>Student Name -- replace</td> 
-            <td className='boldrows'>Status -- replace</td>
+            <td className='boldrows'>{props.name}</td> 
+            {/* <td className='boldrows'>Status -- replace</td> */}
             <td>{props.title}</td>
             <td className='boldrows'>{props.category}</td>
             <td>{timeago.format(props.created_at)}</td>
-            <td><Link to='/'>View</Link></td>
+            <td><Link to={`/Tickets/${props.id}`}>View</Link></td>
+            <Route path='/Tickets/:id' component={Ticket} />
         </>
     
     )
