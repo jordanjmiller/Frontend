0bb767b42c03df5ec4bb662b840ec9f68910188d
George Raymond <georgeraymond98@gmail.com>
2019-11-19 12:36:17 -0500

    made a ticket view component and nav links

---
+++ src/components/Dashboard/TicketView.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+export default function TicketView() {
+  return (
+    <div className="headerDiv">
+      <div className="initial">
+        <Link to="/">
+          {" "}
+          <h1 className="initial">DevDesk</h1>{" "}
+        </Link>
+      </div>
+      <nav className="loggedIn">
+        <NavLink className="navLink" to="/Dashboard/Account">
+          Account
+        </NavLink>
+        <NavLink className="navLink" to="/Dashboard/ReviewTicket"> {/* // Need to make a Component for ReviewTicket */}
+          Review Tickets
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
+
+
