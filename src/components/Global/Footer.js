b8f12603466640d040d45e9972fe2c6a4911238e
Nolan Picini <nolanpic@gmail.com>
2019-11-20 15:29:49 -0800

    Removed unneeded footer props

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Footer(props) {
-    console.log('footer props', props);
+export default function Footer() {
     return (
         <footer>
             <p>Copyright 2019 Lambda DevDesk • <Link to="/credits">Credits</Link></p>
