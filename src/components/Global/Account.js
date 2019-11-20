29183eb0c10ddf44045f5dede6cb0b4d26c37ecf
William Sulinski <w@williamsulinski.com>
2019-11-20 13:07:16 -0800

    Make account work

--- src/components/Global/Account.js
+++ src/components/Global/Account.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import styled from 'styled-components';
+import { axiosWithAuth } from '../../utils/axiosWithAuth';
 
 const Div = styled.div `
     width: 100%;
@@ -11,43 +12,147 @@ const Div = styled.div `
 export default function Account() {
     const { currentUser } = useContext(CurrentUserContext);
 
-    // console.log(currentUser);
+    const [showEditForm, setShowEditForm] = useState(false);
+
+    // state for when the user edits their account details
+    const [editUserName, setEditUserName] = useState('');
+    const [editName, setEditName] = useState('');
+    const [editEmail, setEditEmail] = useState('');
+    const [editCohort, setEditCohort] = useState('');
+    const [editHelper, setEditHelper] = useState(currentUser.helper);
+    const [editStudent, setEditStudent] = useState(currentUser.student);
+
+    console.log(editHelper)
+    // user inputs this so it can be sent to the API in the update request
+    const [inputPassword, setInputPassword] = useState('');
+
+    const handleChange = e => {
+        if (e.target.name === 'username'){
+            setEditUserName(e.target.value);
+        }
+        else if (e.target.name === 'name'){
+            setEditName(e.target.value);
+        }
+        else if (e.target.name === 'email'){
+            setEditEmail(e.target.value);
+        }
+        else if (e.target.name === 'cohort'){
+            setEditCohort(e.target.value);
+        }
+      };
+
+    const passwordChange = (e) => {
+        setInputPassword(e.target.value)
+        console.log(inputPassword)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        let userObj = { password: inputPassword }
+        if (editUserName){
+            userObj = {...userObj, username: editUserName}
+        }
+        if (editName){
+            userObj = {...userObj, name: editName}
+        }
+        if (editEmail){
+            userObj = {...userObj, email: editEmail}
+        }
+        if (editCohort){
+            userObj = {...userObj, cohort: editCohort}
+        }
+
+        console.log(editUserName)
+        console.log(userObj)
+        console.log(inputPassword)
+        axiosWithAuth()
+        .put("https://ddq.herokuapp.com/api/users/user", userObj)
+        
+    }
+
+    const toggleBool = e => {
+        if (e.target.name === "helper") {
+          setEditHelper(!currentUser.helper);
+          
+        } else if (e.target.name === "student") {
+          setEditStudent(!currentUser.student);
+        }
+      };
+
+    
 
     return (
         <Div>
             <h1>Account details</h1>
-            <p>Username: {currentUser.username}</p>
-            <p>Helper? {currentUser.helper === true ? 'Yes' : 'No'}</p>
-            <p>Student? {currentUser.student === true ? 'Yes' : 'No'}</p>
-            <p>Cohort: {currentUser.cohort !== null ? currentUser.cohort : 'Unknown'} </p>
-            <p>Email: {currentUser.email !== null ? currentUser.cohort : 'None'} </p>
 
-            <button>Edit</button>
-            <br />
-            <br />
-            <br />
-            <br />
-{/* This will only render when the edit button is clicked
-            <form>
-                <label> Username:
-                    <input type="text" />
-                </label> 
-                <label> Cohort:
-                    <input type="text" />
-                </label>
-                <label> Email:
-                    <input type="text" />
-                </label>
-                <label> Helper
-                    <input type="checkbox" />
+        {/* Show initially */}
+
+  
+
+{/* Show button */}
+<button onClick={() => setShowEditForm(!showEditForm)}>{showEditForm && 'Cancel'}{!showEditForm && 'Edit'}</button>
+
+            {!showEditForm && <div> 
+                <p>User name: {currentUser.username}</p>
+                <p>Name: {currentUser.name}</p>
+                <p>Email: {currentUser.email !== null ? currentUser.email : 'None'} </p>
+                <p>Cohort: {currentUser.cohort !== null ? currentUser.cohort : 'Unknown'} </p>
+                <br /><br />
+                <p>Helper? {currentUser.helper === true ? 'Yes' : 'No'}</p>
+                <p>Student? {currentUser.student === true ? 'Yes' : 'No'}</p>
+
+            </div>}
+
+
+        {/* Show on button click */}
+            {showEditForm && <form onSubmit={handleSubmit}>
+            
+            <label>User name:
+            <input name="username" onChange={handleChange} placeholder={currentUser.username} type="text"/> 
+        </label>
+        <br /><br />
+            <label>Name:
+                <input name="name" onChange={handleChange} placeholder={currentUser.name} />
+            </label>
+
+            <br /><br />
+
+            <label> Email:
+                <input name="email" type="email" onChange={handleChange} placeholder={currentUser.email !== null ? currentUser.email : ''} />
+            </label> 
+            <br /><br />
+            <label> Cohort:
+                <input name="cohort" type="text" onChange={handleChange} placeholder={currentUser.cohort !== null ? currentUser.cohort : ''} />
+            </label>
+            <br /><br />
+
+            <label> Helper
+                     <input name="helper" type="checkbox" onChange={toggleBool} />
                 </label>
                 <label> Student
-                    <input type="checkbox" />
+                    <input name="student" type="checkbox" checked={currentUser.student} onChange={toggleBool} />
                 </label>
-                    <button>Submit</button>
-            </form> */}
+                <br /><br />
+                <br /><br />
+            <br /><br />
+                <label>
+                    Re-enter password:
+                    <input type='password' name='password' onChange={passwordChange} placeholder='password'/>
+                </label>
+                <br /><br />
+                <button type="submit">Submit changes</button>
 
-            <form>
+            </form> }
+
+        
+
+    
+
+                
+                
+          
+
+            {/* <form>
                 <p>
                     <label> Old password:
                         <input type="text" />
@@ -67,7 +172,7 @@ export default function Account() {
                 </p>
                
                     <button>Submit</button>
-            </form>
+            </form> */}
         </Div>
     )
 }
