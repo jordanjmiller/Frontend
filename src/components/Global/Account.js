7f36539d2f635066c6963b19fd2144b1b13416ca
William Sulinski <w@williamsulinski.com>
2019-11-19 18:41:11 -0800

    Add Account.js

--- src/components/Global/Account.js
+++ src/components/Global/Account.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 export default function Account() {
+    const { currentUser } = useContext(CurrentUserContext);
+
+    console.log(currentUser);
+
     return (
         <div>
-            <h1>This is Account.js</h1>
+            <h1>Account details</h1>
+            {/* <p>Username: {currentUser.username}</p>
+            <p>Helper? {currentUser.helper}</p>
+            <p>Student? {currentUser.student}</p>
+            <p>Cohort: {currentUser.cohort !== null ? currentUser.cohort : 'Unknown'} </p>
+            <p>Email: {currentUser.email !== null ? currentUser.cohort : 'None'} </p> */}
+
+            <button>Edit</button>
+
+{/* This will only render when the edit button is clicked
+            <form>
+                <label> Username:
+                    <input type="text" />
+                </label> 
+                <label> Cohort:
+                    <input type="text" />
+                </label>
+                <label> Email:
+                    <input type="text" />
+                </label>
+                <label> Helper
+                    <input type="checkbox" />
+                </label>
+                <label> Student
+                    <input type="checkbox" />
+                </label>
+                    <button>Submit</button>
+            </form> */}
+
+            <form>
+                <label> Old password:
+                    <input type="text" />
+                </label> 
+                <label> New password:
+                    <input type="text" />
+                </label>
+                <label> Retype new password:
+                    <input type="text" />
+                </label>
+                    <button>Submit</button>
+            </form>
         </div>
     )
 }
