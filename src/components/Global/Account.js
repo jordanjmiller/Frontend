cacaf4e49232ccaa4ca07f7e87b11704d34b1e5d
William Sulinski <w@williamsulinski.com>
2019-11-20 16:41:04 -0800

    Add changepassword to account.js

--- src/components/Global/Account.js
+++ src/components/Global/Account.js
@@ -23,10 +23,11 @@ export default function Account() {
     const [editCohort, setEditCohort] = useState(currentUser.cohort);
     const [editHelper, setEditHelper] = useState(currentUser.helper);
     const [editStudent, setEditStudent] = useState(currentUser.student);
+    const [newPassword, setNewPassword] = useState('');
 
     // console.log(editHelper)
     // user inputs this so it can be sent to the API in the update request
-    const [inputPassword, setInputPassword] = useState('');
+    const [verifyPassword, setVerifyPassword] = useState('');
 
     const handleChange = e => {
         if (e.target.name === 'username'){
@@ -41,17 +42,20 @@ export default function Account() {
         else if (e.target.name === 'cohort'){
             setEditCohort(e.target.value);
         }
+        else if (e.target.name === 'newPassword'){
+            setNewPassword(e.target.value);
+        }
       };
 
     const passwordChange = (e) => {
-        setInputPassword(e.target.value)
-        // console.log(inputPassword)
+        setVerifyPassword(e.target.value)
     }
 
     const handleSubmit = e => {
         e.preventDefault();
+        e.target.reset();
         
-        let userObj = { password: inputPassword }
+        let userObj = { password: verifyPassword }
         if (editUserName){
             userObj = {...userObj, username: editUserName}
         }
@@ -64,14 +68,20 @@ export default function Account() {
         if (editCohort){
             userObj = {...userObj, cohort: editCohort}
         }
+        if (newPassword){
+            userObj = {...userObj, newPassword: newPassword}
+        }
+        
 
         // console.log(editUserName)
         // console.log(userObj)
-        // console.log(inputPassword)
+        
         if (validateInputs()) {
         axiosWithAuth()
         .put("https://ddq.herokuapp.com/api/users/user", userObj)
-        .then(alert("Account update successful"))
+        .then(res => {
+            console.log(res);
+        })
         .catch(err => {
             console.log("Edit Account Catch Error: ", err.response.data.message);
             alert(err.response.data.message);
@@ -82,15 +92,40 @@ export default function Account() {
 
     // this isn't working 
 
-    const toggleBool = e => {
-        if (e.target.name === "helper") {
-          setEditHelper(!currentUser.helper);
+    // const toggleBool = e => {
+    //     if (e.target.name === "helper") {
+    //       setEditHelper(!currentUser.helper);
           
-        } else if (e.target.name === "student") {
-          setEditStudent(!currentUser.student);
-        }
-      };
+    //     } else if (e.target.name === "student") {
+    //       setEditStudent(!currentUser.student);
+    //     }
+    //   };
 
+    const isValidPassword = password => {
+    if (password === "") {
+        alert("You must enter a password.");
+        return false;
+    }
+    if (password.length < 5 || password.length > 20) {
+        alert("Password cannot be less than 5 or greater than 20 characters");
+        return false;
+    }
+    if (!/(!|@|#|\$|&|\*|%|^)/.test(password)) {
+        alert("Password must contain at least one special character");
+        return false;
+    }
+    if (!/([A-Z])/.test(password)) {
+        alert("Password must contain at least one capitalized letter");
+        return false;
+    }
+    if (!/([0-9])/.test(password)) {
+        alert("Password must contain at least one number");
+        return false;
+    }
+    return true;
+    };
+
+      
     const validateInputs = () => {
     if (editUserName === "") {
         alert("Your username can't be empty.");
@@ -104,10 +139,14 @@ export default function Account() {
         alert("You must enter your name.");
         return false;
     }
-    if (editHelper === false && editStudent === false) {
-        alert("You must choose to enroll as a helper, student, or both.");
-        return false;
-    }
+    // if (editHelper === false && editStudent === false) {
+    //     alert("You must choose to enroll as a helper, student, or both.");
+    // //     return false;
+    // }
+    // if (!isValidPassword(newPassword)) {
+    //     return false;
+    //   }
+    
     return true;
     };
 
@@ -132,8 +171,8 @@ export default function Account() {
                 <p>Email: {currentUser.email !== null ? currentUser.email : 'None'} </p>
                 <p>Cohort: {currentUser.cohort !== null ? currentUser.cohort : 'Unknown'} </p>
                 <br /><br />
-                <p>Helper? {currentUser.helper === true ? 'Yes' : 'No'}</p>
-                <p>Student? {currentUser.student === true ? 'Yes' : 'No'}</p>
+                {/* <p>Helper? {currentUser.helper === true ? 'Yes' : 'No'}</p>
+                <p>Student? {currentUser.student === true ? 'Yes' : 'No'}</p> */}
 
             </div>}
 
@@ -141,6 +180,9 @@ export default function Account() {
         {/* Show on button click */}
             {showEditForm && <form onSubmit={handleSubmit}>
             
+        
+        <br /><br />
+
             <label>User name:
             <input name="username" onChange={handleChange} placeholder={currentUser.username} type="text"/> 
         </label>
@@ -160,13 +202,16 @@ export default function Account() {
             </label>
             <br /><br />
 
-            <label> Helper
+            {/* <label> Helper
                      <input name="helper" type="checkbox" onChange={toggleBool} />
                 </label>
                 <label> Student
                     <input name="student" type="checkbox" checked={currentUser.student} onChange={toggleBool} />
-                </label>
+                </label> */}
                 <br /><br />
+                <label>New password:
+            <input name="newPassword" onChange={handleChange} placeholder='' type="text"/> 
+        </label>
                 <br /><br />
             <br /><br />
                 <label>
